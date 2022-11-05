d9b7f3538ed71efc75419f59f0963470de6f3f81
easyoung <slhyj95@gmail.com>
2022-11-06 00:49:25 +0900

    ssalendar 초기설정

---
+++ src/main/java/com/ssafy/ssalendar/SsalendarApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsalendarApplication.class, args);
+	}
+
+}
