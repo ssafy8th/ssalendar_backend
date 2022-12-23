7d037ddc85196bc03472fa6798da8bd112ccd70d
jehyuck <wpgur07@gmail.com>
2022-12-23 12:33:04 +0900

    fix: swaggerConfig 에러 구문 수정

--- src/main/java/com/ssafy/ssalendar/config/SwaagerConfig.java
+++ src/main/java/com/ssafy/ssalendar/config/SwaagerConfig.java
@@ -1,6 +1,5 @@
 package com.ssafy.ssalendar.config;
 
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,8 +11,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
